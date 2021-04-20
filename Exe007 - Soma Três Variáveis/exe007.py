199c46cda1200a21b1f9f0bf4b3cdc28ce81c2a1
SouAline <ds.alinesouza@gmail.com>
2021-04-19 23:29:03 -0300

    Exercício para prática

---
+++ Exe007 - Soma Três Variáveis/exe007.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite um número inteiro: '))
+n3 = float(input('Digite um número real: '))
+dn1 = n1 + n1
+trin1 = n1 * 3
+print('O dobro do {} mais a metade de {} é igual a {}'.format(n1, n2, dn1+(n2/2)))
+print('A soma do triplo de {} com o {} é igual a {}'.format(n1, n3, (trin1 + n3)))
+print('O número {} elevado ao cubo é igual a {}'.format(n3, n3**3))
